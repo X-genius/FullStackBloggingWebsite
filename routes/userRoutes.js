5661d6390d810f254be735f11e864aef5421428f
Aman <55349238+X-genius@users.noreply.github.com>
2021-01-20 19:50:18 +0530

    remove files

--- routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const app = require('express');
-const {register , registerValidator , login , loginValidator} = require("../controllers/userController");
-const router =  app.Router();
-router.post("/register" , registerValidator , register);
-router.post("/login" , loginValidator , login);
-module.exports = router;
